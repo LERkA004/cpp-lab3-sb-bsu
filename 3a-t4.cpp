7206a9ea5b82539fb8c3df3a89a8ffa5155aff72
LERkA004 <95502830+LERkA004@users.noreply.github.com>
2021-12-03 23:27:25 +0300

    Create 3a-t4.cpp

---
+++ 3a-t4.cpp
@@ -0,0 +1,20 @@
+#include<iostream> 
+using namespace std; 
+
+int main()
+{
+int x, y = 1, sum_x = 0, number;
+cout << " Enter the amount of integers = "; cin >> x;
+if (k x 0)
+{ 
+    do {
+    cout << "Enter integer number " << y << ": "; 
+    cin >> number;
+    sum_x += number; 
+    i++;
+    } while (y <= x);
+    
+}
+cout << " The total sum of " << x << " integers is: "  << sum_x;
+return 0;
+}
